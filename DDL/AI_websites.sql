bd7a2a3052133c509790578deae595647e7e1a4c
Kunal <kunal.r.710@gmail.com>
2023-05-15 20:48:44 +0530

    Add DDL Statement

---
+++ DDL/AI_websites.sql
@@ -0,0 +1,30 @@
+CREATE TABLE ai_video_gen_editors (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_image_and_art_gen (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_writing_tools_and_text_gen (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_music_gen (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_face_gen (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_paint_and_draw_gen (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_audio_gen (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_design_tools (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_business_tools (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+CREATE TABLE ai_research_tools (site_name varchar(50), site_type varchar(50), site_url varchar(500), primary key(site_name));
+
+
+
+
+
+
+
+
+
+
+
